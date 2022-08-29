faea2bc56906a3cc83bfdc4793c421e27bc9f441
Marius Sili <sili.marius.me@gmail.com>
2022-08-29 12:48:27 +0200

    Wrote tests for dice roll endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
 export const PORT = Number(process.env.PORT) || "5001";
+export const BASE_URL = `localhost:${PORT}`;
 export const CORS_ORIGINS = process.env.CORS_ORIGINS?.split(",") || ["http://localhost:3000", "http://localhost:3001"];
