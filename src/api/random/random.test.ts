a9f69d2e323cdc307b9a3c45aaa36854a5ae21d0
Marius Sili <sili.marius.me@gmail.com>
2022-08-29 13:48:39 +0200

    Created tests for random api endpoint

---
+++ src/api/random/random.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+
+import { BASE_URL } from "../../constants";
+
+jest.setTimeout(60000);
+
+describe("POST /random/generate", () => {
+  const route = "/random/generate";
+
+  it("should return 400 when req body is empty", async () => {
+    const res = await request(BASE_URL).post(route);
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return 400 when max < min", async () => {
+    const res = await request(BASE_URL).post(route).send({ min: 3, max: 2 });
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return 200", async () => {
+    const res = await request(BASE_URL).post(route).send({ min: 0, max: 7 });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.error).toBeUndefined();
+  });
+});
--- src/api/random/schemas.ts
+++ src/api/random/schemas.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
-export const GenerateBody = z.object({
-  min: z.number().positive(),
-  max: z.number().positive(),
-});
+export const GenerateBody = z
+  .object({
+    min: z.number(),
+    max: z.number(),
+  })
+  .refine((schema) => schema.min < schema.max, { message: "max has to be greater than min" });
