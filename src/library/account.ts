6cb7bc15eda503b43f1b29fd0fb470361ed84fb5
Marius Sili <sili.marius.me@gmail.com>
2022-09-19 13:59:39 +0200

    Removed todo

--- src/library/account.ts
+++ src/library/account.ts
@@ -1,7 +1,6 @@
 import { aleoService } from "../services";
 
 export const account = {
-  // TODO: store account information somewhere
   create: async () => {
     const res = await aleoService.createAccount();
     return res;
