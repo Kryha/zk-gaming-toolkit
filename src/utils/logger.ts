899666f329616ae2344fa74d49342f06935e83bf
Silviu Marii <silviu@kryha.io>
2023-05-04 11:37:36 +0000

    Merged PR 4132: Remove sensible content
    Removed sensible content form the logger.
    
    ###QA
    When making a toolkit request (roll Dice or get power-up) the logger should censor the viewKey, privateKey and the owner.
    
    Related work items: #17988

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,11 +4,16 @@ import { env } from "../constants";
 
 const logLevel = env.NODE_ENV === "production" ? "http" : "debug";
 
+const SENSITIVE_PROPS = [/"aleo1[^"]*/g, /aleo1[^ ]*/g, /aleo1[^$]*/g, /"APrivateKey1[^"]*/g, /"AViewKey1[^"]*/g];
+
 export const logger = createLogger({
   level: logLevel,
   format: format.combine(
     format.timestamp(),
     format.printf(({ level, message, timestamp }) => {
+      SENSITIVE_PROPS.forEach((prop) => {
+        message = message.replace(prop, '"***"');
+      });
       return `${timestamp} ${level.slice(0, 7).padEnd(7, " ")} - ${message}`;
     })
   ),
